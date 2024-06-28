9c2e8e3d56c2a8f4a9058e99f3a5adf32d9a6318
Charles-cdel <lionbllack31@gmail.com>
2024-06-28 15:52:53 -0300

    subindo tudo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+ 
+
+  return (
+    <div className="container">
+      <h2>Calculo de Média</h2>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Button.jsx
@@ -0,0 +1,8 @@
+export default function Button(){
+    return(
+        <div className="buttons">
+        <button>Calcular</button>
+        
+      </div>
+    )
+}
